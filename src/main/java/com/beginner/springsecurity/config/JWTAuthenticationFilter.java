b3e06e801071c23ccf5f7b90aeacf6413833bed9
Ahamed-Ismail <ahamedismailhisamm@gmail.com>
2025-06-17 23:34:20 +0530

    Added few files

---
+++ src/main/java/com/beginner/springsecurity/config/JWTAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.beginner.springsecurity.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+
+
+    }
+}
---
+++ src/main/java/com/beginner/springsecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.beginner.springsecurity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
